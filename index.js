d0c12aedea4736af72c28cb04e3c8ce05f86328d
Fernando Bueno <fernando_jordi@hotmail.com>
2022-08-02 18:44:28 -0300

    adiciona o requisito 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,20 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 const data = './talker.json';
+
+function validateLogin(req, res, next) {
+  const { password, email } = req.body;
+
+  const validateEmail = /\S+@\S+\.\S+/;
+
+  if (password.length < 4 || password.length > 8) {
+    return res.status(400).json({ message: 'Invalid password!' });
+  } 
+  if (validateEmail.test(email) === false) {
+    return res.status(400).json({ message: 'Invalid email!' });
+  } 
+  next();
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -27,6 +42,11 @@ app.get('/talker/:id', async (req, res) => {
   const talker = JSON.parse(talkers).find((t) => t.id === Number(id));
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(200).json(talker);
+});
+
+app.post('/login', validateLogin, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
