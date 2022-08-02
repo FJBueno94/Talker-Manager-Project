9458e6cb52e776b70627c026f737ca278e516e88
Fernando Bueno <fernando_jordi@hotmail.com>
2022-08-02 17:32:26 -0300

    adiciona o requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,17 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const data = './talker.json';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(data, 'utf8');
+  if (talkers === '') return res.status(200).json([]);
+  return res.status(200).json(JSON.parse(talkers));
 });
 
 app.listen(PORT, () => {
